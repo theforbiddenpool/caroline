944fff699294bf4ba045c08836ba9d098086c963
theforbiddenpool <contact@theforbiddenpool.com>
2022-07-27 17:57:49 +0200

    fix(seeds): remove alter sequence query

--- db/seed.ts
+++ db/seed.ts
@@ -9,9 +9,6 @@ const load = async () => {
     await prisma.food.deleteMany();
     console.log('deleted records in foods table');
 
-    await prisma.$queryRaw`UPDATE sqlite_sequence SET seq = 1 WHERE name = foods`;
-    console.log('reset foods auto increment to 1');
-
     foods.map(async (food) => {
       await prisma.food.create({
         data: food,
