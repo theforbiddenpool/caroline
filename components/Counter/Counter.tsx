9d59eb509f48336f33cc29b1c090f38240d22fbe
theforbiddenpool <contact@theforbiddenpool.com>
2022-07-28 13:13:32 +0200

    feat(counter): handle change on input

--- components/Counter/Counter.tsx
+++ components/Counter/Counter.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { IconMinus, IconMinusVertical, IconPlus } from '@tabler/icons';
 
 interface CounterProps {
@@ -5,12 +6,35 @@ interface CounterProps {
 }
 
 function Counter({ total }: CounterProps) {
+  const [serving, setServing] = useState<number | ''>('');
+
+  function handleKeypress(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'Backspace' || e.key === 'Delete') {
+      if (String(serving).length === 1) {
+        setServing('');
+      } else {
+        setServing(Number(e.currentTarget.value));
+      }
+    }
+
+    if (/\b[a-z]\b/i.test(e.key)) {
+      e.preventDefault();
+    }
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setServing(Number(e.currentTarget.value));
+  }
+
   return (
     <div>
       <span>
         <button type="button" aria-label="decrement"><IconMinus size={13} role="presentation" /></button>
         <input
           type="number"
+          value={serving}
+          onChange={handleChange}
+          onKeyDown={handleKeypress}
           step="0.5"
           className="w-14 mx-3 text-center border-2 border-gray-900"
           aria-label="servings"
