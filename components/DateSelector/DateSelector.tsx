61f5a42fcc065501e05d94df2bc101cc28804f4c
theforbiddenpool <contact@theforbiddenpool.com>
2022-07-29 11:15:23 +0200

    feat(date): switch date functionality

--- components/DateSelector/DateSelector.tsx
+++ components/DateSelector/DateSelector.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import { IconChevronLeft, IconChevronRight } from '@tabler/icons';
-import { formatDate } from './date';
+import { formatDate, getNextDate, getPreviousDate } from './date';
 
 function DateSelector() {
+  const [current, setCurrent] = useState(new Date());
+
+  function handlePrevious() {
+    setCurrent(getPreviousDate(current));
+  }
+
+  function handleNext() {
+    setCurrent(getNextDate(current));
+  }
+
   return (
     <nav className="flex justify-end p-3 bg-lime-200">
-      <button type="button" className="align-middle p-1" aria-label="previous">
+      <button type="button" onClick={handlePrevious} className="align-middle p-1" aria-label="previous">
         <IconChevronLeft size={15} className="" role="presentation" />
       </button>
-      <span className="text-center min-w-100 mx-1">{formatDate(new Date())}</span>
-      <button type="button" className="align-middle p-1" aria-label="next">
+      <span className="text-center min-w-100 mx-1">{formatDate(current)}</span>
+      <button type="button" onClick={handleNext} className="align-middle p-1" aria-label="next">
         <IconChevronRight size={15} className="align-bottom" role="presentation" />
       </button>
     </nav>
--- components/DateSelector/date.ts
+++ components/DateSelector/date.ts
@@ -8,4 +8,18 @@ export function formatDate(date: Date, options?: Intl.DateTimeFormatOptions) {
   return date.toLocaleDateString('en-US', dateOptions);
 }
 
-export default { formatDate };
+export function getPreviousDate(date: Date) {
+  const previous = new Date(date.getTime());
+  previous.setDate(date.getDate() - 1);
+
+  return previous;
+}
+
+export function getNextDate(date: Date) {
+  const next = new Date(date.getTime());
+  next.setDate(date.getDate() + 1);
+
+  return next;
+}
+
+export default { formatDate, getPreviousDate, getNextDate };
