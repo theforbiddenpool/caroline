ee07320e0edc1ff3da016bf35f158b81e2bb329f
theforbiddenpool <contact@theforbiddenpool.com>
2022-08-02 14:04:39 +0200

    test(header): initial tests + add handler for user with no name

--- mocks/api/handlers.ts
+++ mocks/api/handlers.ts
@@ -3,7 +3,16 @@ import { rest } from 'msw';
 export const sessionHandler = rest.get('/api/auth/session', (req, res, ctx) => res(ctx.json({
   user: {
     name: 'john',
-    email: 'john@localhost.com',
+    email: 'john@example.com',
+    image: null,
+  },
+  expires: '2022-09-01T09:48:41.522Z',
+})));
+
+export const sessionHandlerNoName = rest.get('/api/auth/session', (req, res, ctx) => res(ctx.json({
+  user: {
+    name: null,
+    email: 'john@example.com',
     image: null,
   },
   expires: '2022-09-01T09:48:41.522Z',
