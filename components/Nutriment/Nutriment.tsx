e62b94cec19476b1b50e902be5b23d0411783852
theforbiddenpool <contact@theforbiddenpool.com>
2022-08-16 12:09:36 +0200

    refactor(nutriment): rename props interface to match the component name

--- components/Nutriment/Nutriment.tsx
+++ components/Nutriment/Nutriment.tsx
@@ -1,11 +1,11 @@
 import { Food } from '@prisma/client';
 import Counter from './Counter/Counter';
 
-interface FoodProps {
+interface NutrimentProps {
   data: Food
 }
 
-function Nutriment({ data }: FoodProps) {
+function Nutriment({ data }: NutrimentProps) {
   return (
     <div className="flex bg-zinc-50 p-5">
       <h3 className="flex-grow capitalize">{data.name}</h3>
