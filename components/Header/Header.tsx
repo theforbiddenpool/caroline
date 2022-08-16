530810d36b853ddc37bd30e848b1c4f1f0208469
theforbiddenpool <contact@theforbiddenpool.com>
2022-08-16 13:59:22 +0200

    refactor(nav): separate nav into authenticated and unauthenticated navs

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,50 +1,18 @@
-import { signIn, signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import { UnauthenticatedNav, AuthenticatedNav } from './Nav';
 
 function Header() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   return (
-    <header className="w-full flex items-center bg-lime-300 p-5 mb-10">
-      <h1 className="text-3xl font-medium flex-grow">Caroline</h1>
-      {session?.user && (
-      <p className="mr-4">
-        Signed in as
-        {' '}
-        {session.user.name ?? session.user.email}
-      </p>
+    <header className="w-full flex justify-between items-center bg-lime-300 p-5 mb-10">
+      <h1 className="text-3xl font-medium">Caroline</h1>
+      {status !== 'loading' && (
+        <>
+          {!session && <UnauthenticatedNav />}
+          {session?.user && <AuthenticatedNav user={session.user} />}
+        </>
       )}
-      <nav>
-        <ul>
-          {!session && (
-          <li>
-            <Link
-              href="/api/auth/signin"
-              onClick={(e) => {
-                e.preventDefault();
-                signIn();
-              }}
-            >
-              Sign In
-            </Link>
-          </li>
-          )}
-
-          {session?.user && (
-          <li>
-            <Link
-              href="/api/auth/signout"
-              onClick={(e) => {
-                e.preventDefault();
-                signOut();
-              }}
-            >
-              Logout
-            </Link>
-          </li>
-          )}
-        </ul>
-      </nav>
     </header>
   );
 }
---
+++ components/Header/Nav/index.ts
@@ -0,0 +1,2 @@
+export { default as AuthenticatedNav } from './AuthenticatedNav';
+export { default as UnauthenticatedNav } from './UnauthenticatedNav';
