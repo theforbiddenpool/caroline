5e9f4967dac7b44c4d73d6cb24b2a136f2761725
theforbiddenpool <contact@theforbiddenpool.com>
2022-08-18 11:50:28 +0200

    fix(types): remove redundant property intersection

--- types/types.d.ts
+++ types/types.d.ts
@@ -2,8 +2,6 @@ import type { DefaultUser } from 'next-auth';
 
 declare module 'next-auth' {
   interface Session {
-    user?: DefaultUser & {
-      id: string;
-    };
+    user?: DefaultUser;
   }
 }
