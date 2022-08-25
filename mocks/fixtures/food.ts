4d6ff08eee7264b82bbe6dac972f406b1cc3e2c5
theforbiddenpool <contact@theforbiddenpool.com>
2022-08-25 21:34:46 +0200

    test(fixtures): change food ids to human-readable ones

--- mocks/fixtures/food.ts
+++ mocks/fixtures/food.ts
@@ -1,21 +1,21 @@
 import { Food } from '@prisma/client';
 
 export const beans: Food = {
-  id: 'cl6xqxw8v0005o9m0f7do4q5x',
+  id: 'beans1',
   name: 'beans',
   quantity: 3,
   order_weight: 1,
 };
 
 export const berries: Food = {
-  id: 'cl6xqxw8v0004o9m0nei0s0t1',
+  id: 'berries2',
   name: 'berries',
   quantity: 1,
   order_weight: 2,
 };
 
 export const nutsAndSeeds: Food = {
-  id: 'cl6xqxw8v0018o9m0aawyvnr9',
+  id: 'nutsandseeds3',
   name: 'nuts and seeds',
   quantity: 1,
   order_weight: 3,
