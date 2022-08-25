4e68f58345095e4e11c04fc5267ffbed4c5386e0
theforbiddenpool <contact@theforbiddenpool.com>
2022-08-25 20:12:33 +0200

    test(fixtures): add types

--- mocks/fixtures/food.ts
+++ mocks/fixtures/food.ts
@@ -1,25 +1,27 @@
-export const beans = {
+import { Food } from '@prisma/client';
+
+export const beans: Food = {
   id: 'cl6xqxw8v0005o9m0f7do4q5x',
   name: 'beans',
-  total: 3,
+  quantity: 3,
   order_weight: 1,
 };
 
-export const berries = {
+export const berries: Food = {
   id: 'cl6xqxw8v0004o9m0nei0s0t1',
   name: 'berries',
-  total: 1,
+  quantity: 1,
   order_weight: 2,
 };
 
-export const nutsAndSeeds = {
+export const nutsAndSeeds: Food = {
   id: 'cl6xqxw8v0018o9m0aawyvnr9',
   name: 'nuts and seeds',
-  total: 1,
+  quantity: 1,
   order_weight: 3,
 };
 
-export const all = [
+export const all: Food[] = [
   beans,
   berries,
   nutsAndSeeds,
--- mocks/fixtures/user.ts
+++ mocks/fixtures/user.ts
@@ -1,4 +1,11 @@
-export const john = {
+import { User } from '@prisma/client';
+
+interface UserSession {
+  user: Pick<User, 'id' | 'name' | 'email' | 'image'>;
+  expires: Date | string;
+}
+
+export const john: UserSession = {
   user: {
     id: 'cl6yukoqn0010tfm0ncina1nw',
     name: 'john',
@@ -8,7 +15,7 @@ export const john = {
   expires: '2022-09-01T09:48:41.522Z',
 };
 
-export const johnNoName = {
+export const johnNoName: UserSession = {
   ...john,
   user: {
     ...john.user,
