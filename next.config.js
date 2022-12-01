7933a76ea82ce70efb948a02df0b47cc12ac9677
theforbiddenpool <contact@theforbiddenpool.com>
2022-12-01 13:26:05 +0100

    chore(localization): install and configure next-i18next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
+const nextI18NextConfig = require('./next-i18next.config');
+
 module.exports = {
   reactStrictMode: true,
   experimental: {
     newNextLinkBehavior: true,
   },
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
+  i18n: nextI18NextConfig.i18n,
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
-import '../i18n/config';
+import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
@@ -10,4 +10,4 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     </SessionProvider>
   );
 }
-export default MyApp;
+export default appWithTranslation(MyApp);
